e9d2723ac7b937fdf869b3f63792daca07885ab5
VytRib <vytenris@gmail.com>
2025-12-01 23:33:53 +0200

    Add migration to sync model with code

---
+++ Migrations/20251201213332_SyncModelWithCode.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameReviewsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelWithCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Games_GameId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_GameId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_GenreId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Games",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Games",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Games",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Games",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_GameId",
+                table: "Reviews",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GenreId",
+                table: "Games",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genres_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Games_GameId",
+                table: "Reviews",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
